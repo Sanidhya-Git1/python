d07ab1ee7bbe410e776efb33bb2cdf5ee4dfac17
= <sanidhyasoniit@gmail.com>
2026-08-08 17:55:23 -0500

    Variables, Datatypes, Operators

---
+++ variables_operators.py
@@ -0,0 +1,20 @@
+# variables
+x = 10
+y = 20
+name = 'Alice'
+d = True
+
+#Arithmetic operators
+print("x + y", x + y)
+
+# comparison operators
+print("x == y ?", x == y)
+print (" x < y ?", x < y)
+
+# logical operators
+print ("x > 0 and y > 0 : ", x > 0 and y > 0)
+
+#other operators
+print("not x <  0", not x < 0)
+print(" 'A' in name", 'A' in name)
+print("x is y : ", x is y );
