a27b9b477ff393526d904f0d523f86d37e963243
= <sanidhyasoniit@gmail.com>
2026-08-09 23:19:49 -0500

    module

--- functions.py
+++ functions.py
@@ -19,7 +19,7 @@ greet(message="Good Morning", name="Ekta")
 # ---- Function parameter and Argument
 
 # variable length argument
-# 1. *args
+# 1. *args -- allows a function to accept any number of positional arguments and it collects all positional arguments into a tuple
 def multiply(*args):
     res = 1
     for num in args:
@@ -28,7 +28,7 @@ def multiply(*args):
 
 print(multiply(1,2,3,4))
 
-# 2. **kwargs
+# 2. **kwargs -- Allows a function to accept any number of keyword arguments. It collects all keyword arguments into a dictionaly.
 def print_info(**kwargs):
     for key,value in kwargs.items():
         print(f"{key} : {value}")
---
+++ mymodule.py
@@ -0,0 +1,7 @@
+# Module : A module is a file containing Python code. It can define functions, classes and variables. You can use it to organize your code into reusable pieces.
+def greet(name):
+    return f"Hello, {name}"
+
+def add(a, b):
+    return a+b
+
