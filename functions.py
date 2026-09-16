eb46f777cdd95f4f5c584fce885e0dbb2d10c9bd
= <sanidhyasoniit@gmail.com>
2026-09-16 16:24:22 -0500

    Type hinting in a function

--- functions.py
+++ functions.py
@@ -28,10 +28,27 @@ def multiply(*args):
 
 print(multiply(1,2,3,4))
 
-# 2. **kwargs -- Allows a function to accept any number of keyword arguments. It collects all keyword arguments into a dictionaly.
+# 2. **kwargs -- Allows a function to accept any number of keyword arguments. It collects all keyword arguments into a dictionayy.
 def print_info(**kwargs):
     for key,value in kwargs.items():
         print(f"{key} : {value}")
 
 print_info(name="Alice", age=30, city='New York')
 
+
+# Type Hinting 
+# It is not forced in Python but it is a good practice to use type hints to indicate the expected types of function parameters and return values. It helps with code readability and can assist with static type checking.
+def greet_someone(name: str) -> None :   
+    print(f"Hello, {name}!")
+
+greet_someone("Alice")  # Output: Hello, Alice!
+greet_someone(name = "Bob")    # Output: Hello, Bob!
+
+def add_numbers(a : int, b : int) -> int : 
+    return a + 2 * b;
+
+result = add_numbers(3, 4)  # Output: 11
+print(result)
+
+result = add_numbers(a=5, b=6)  # Output: 17
+print(result)
